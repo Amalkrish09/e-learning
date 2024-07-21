2ece0f4722de6afd9f8fa18b0aeb48be251dbebb
Amal krishnan <amal@petroinfotech.com>
2024-07-21 16:15:22 +0400

    login

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,5 +1,5 @@
 import { Routes } from '@angular/router';
-import { LoginComponent } from './web-app/login/login.component';
+import { LoginComponent } from './login/login.component';
 
 export const routes: Routes = [
      { path: '', redirectTo: '/web-app/home', pathMatch: 'full' },
